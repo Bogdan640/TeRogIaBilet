9171ad82b48ed7cf101ca746eb0d434df451a988
Bogdan640 <bogdanbubu2004@gmail.com>
2025-03-19 17:11:59 +0200

    Day 2 : 17:11....check, all good here

---
+++ src/In_memory_storage/Concerts.js
@@ -0,0 +1,17 @@
+
+const concerts = [
+    { id: 1, name: "Rock Night", genre: "Rock", price: "$30", location: "London", date: "2025-04-12" },
+    { id: 2, name: "Metal Madness", genre: "Metal", price: "$40", location: "Berlin", date: "2025-06-23" },
+    { id: 3, name: "Jazz Vibes", genre: "Jazz", price: "$25", location: "New Orleans", date: "2025-05-14" },
+    { id: 4, name: "Hip-Hop Beats", genre: "Hip-Hop", price: "$35", location: "New York", date: "2025-07-10" },
+    { id: 5, name: "Electronic Fusion", genre: "EDM", price: "$50", location: "Amsterdam", date: "2025-08-21" },
+    { id: 6, name: "Classic Symphony", genre: "Classical", price: "$20", location: "Vienna", date: "2025-09-30" },
+    { id: 7, name: "Indie Magic", genre: "Indie", price: "$28", location: "San Francisco", date: "2025-10-15" },
+    { id: 8, name: "Punk Attack", genre: "Punk", price: "$38", location: "Los Angeles", date: "2025-11-05" },
+    { id: 9, name: "Reggae Sunset", genre: "Reggae", price: "$22", location: "Jamaica", date: "2025-12-20" },
+    { id: 10, name: "Pop Extravaganza", genre: "Pop", price: "$45", location: "Tokyo", date: "2026-01-10" },
+    { id: 11, name: "Country Road", genre: "Country", price: "$27", location: "Texas", date: "2026-02-14" },
+    { id: 12, name: "Techno Festival", genre: "Techno", price: "$55", location: "Berlin", date: "2026-03-03" }
+];
+
+export default concerts;
