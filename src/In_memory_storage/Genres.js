503e54aa48cdf841006da294cec18911577e75d5
Bogdan640 <bogdanbubu2004@gmail.com>
2025-03-19 21:43:57 +0200

    Day 2 : 21:40  By quill and key, the digital front shifts. Though shadows linger, a sliver of code-won ground is ours this night.

---
+++ src/In_memory_storage/Genres.js
@@ -0,0 +1,16 @@
+const genresList = [
+    "Hard Rock",
+    "Alternative Rock",
+    "Pop",
+    "Jazz",
+    "Electronic",
+    "Metal",
+    "Blues",
+    "Hip Hop",
+    "Country",
+    "Classical",
+    "Indie",
+    "Reggae"
+];
+
+export default genresList;
